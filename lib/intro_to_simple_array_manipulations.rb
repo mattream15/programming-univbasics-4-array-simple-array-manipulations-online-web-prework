2569f0dd088ced6d69e7e760005e826df35f74b0
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 15:39:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,8 @@ artists(array, string) = ["Hozier", "Ariana Grande", "Usher"]
 next_artist = "Beyonce"
 artists(array, string).push(next_artist)
 end
+
+def artists(array, string)
+  artists(array, string) = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists(array, string).pop
+end
